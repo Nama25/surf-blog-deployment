9b1301b250c672f6d5f85787f7522d718e2d10f0
Nadine Machmeier <info@nadinemachmeier.com>
2023-01-25 23:06:28 +0100

    login, signup routes, post route for create user profile

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -20,14 +20,42 @@ router.get("/login", (req, res) => {
   res.render("auth/login");
 });
 
+// POST route
+router.post("/login", (req, res) => {
+  console.log('SESSION =====> ', req.session);
+  console.log(req.body)
+  const {email, password} = req.body
+
+  if (!email || !password) {
+    res.render("auth/login", {
+      errorMessage:"Please enter your email and password to surf."
+    })
+    return;
+  }
+  User.findOne({email})
+  .then(user => {
+    console.log(user)
+    if(!user) {
+      res.render("auth/login", {errorMessage: "E-Mail is not registered - please signup "})
+    } else if (bcrypt.compareSync(password, user.passwordHash))  {
+      req.session.currentUser = user
+      res.redirect("/user-profile")
+    } else {
+      res.render("auth/login", {errorMessage: "Incorrect password"})
+    }
+    })
+    .catch(error => next(error))
+
+})
+
 // SIGNUP ROUTES
 // GET route
-router.get("/signup", (req, res) => {
+router.get("/signup", isLoggedOut, (req, res) => {
   res.render("auth/signup");
 });
 
 // POST route
-router.post("/signup", (req, res, next) => {
+router.post("/signup", isLoggedOut, (req, res, next) => {
   console.log(req.body);
   const { username, email, password } = req.body;
   bcrypt
@@ -45,7 +73,7 @@ router.post("/signup", (req, res, next) => {
       })
         .then((result) => {
           console.log("New user created:", result);
-          res.redirect("/user-profile");
+          res.redirect("/create-user-profile");
         })
         .catch((err) => {
           console.log(err);
@@ -55,14 +83,39 @@ router.post("/signup", (req, res, next) => {
 
 // USER PROFILE ROUTES
 // GET route
-router.get("/user-profile", (req, res) => {
-  res.render("user/user-profile");
+router.get("/user-profile", isLoggedIn, (req, res) => {
+  res.render("user/user-profile", { userInSession: req.session.currentUser });
 });
 
 // CREATE USER PROFILE route
 // GET route
+// Do we have to add Middleware??????
 router.get("/create-user-profile", (req, res) => {
-  res.render("user/create-user-profile");
+  const surfingLevel = ["Beginner", "Intermediate", "Advanced"]
+  const surfingType = ["Surfing", "Body Surfing", "Body Boarding"]
+  res.render("user/create-user-profile", {surfingLevel, surfingType});
+});
+
+//POST route
+router.post('/create-user-profile', (req, res) => {
+  console.log(req.body)
+
+  const {profileImage, surfLevel, typeOfSurfing, favoriteSpots} = req.body
+
+  UserProfile.create({
+    profileImage: profileImage, surfLevel: surfLevel, typeOfSurfing: typeOfSurfing, favoriteSpots: favoriteSpots
+  })
+  .then( ()=> {
+    res.redirect("/user-profile");
+  })
+  .catch (err => console.log(err))
+})
+
+
+// EDIT USER PROFILE route
+// GET route
+router.get("/user-profile/edit", isLoggedIn, (req, res) => {
+  res.render("user/edit-user-profile", { userInSession: req.session.currentUser });
 });
 
 // ALL SURF SPOTS ROUTES
@@ -73,14 +126,20 @@ router.get("/all-surf-spots", (req, res) => {
 
 // CREATE SURF SPOTS ROUTES
 // GET route
-router.get("/create-surf-spot", (req, res) => {
-  res.render("surf-spots/create-surf-spot");
+router.get("/create-surf-spot", isLoggedIn, (req, res) => {
+  res.render("surf-spots/create-surf-spot", { userInSession: req.session.currentUser });
 });
 
 // SURF SPOT PROFILE ROUTES
 // GET route
-router.get("/surf-spot-profile/:id", (req, res) => {
-  res.render("surf-spots/surf-spot-profile");
+router.get("/surf-spot-profile/:id", isLoggedIn, (req, res) => {
+  res.render("surf-spots/surf-spot-profile", { userInSession: req.session.currentUser });
+});
+
+// EDIT SURF SPOT PROFILE ROUTES
+// GET route
+router.get("/surf-spot-profile/:id/edit", isLoggedIn, (req, res) => {
+  res.render("surf-spots/edit-surf-spot", { userInSession: req.session.currentUser });
 });
 
 module.exports = router;
