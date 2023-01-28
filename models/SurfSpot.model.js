b29c5f10ceb6eedd00364d929205b760dee08066
AzizaHalder <aziza.halder@sap.com>
2023-01-28 18:25:43 +0100

    Edit surf get route and hbs

--- models/SurfSpot.model.js
+++ models/SurfSpot.model.js
@@ -1,71 +1,74 @@
 const { Schema, model } = require("mongoose");
-const surfSpotSchema = new Schema({
-  spotImage: {
-    type: String,
-    // required: true,
+const surfSpotSchema = new Schema(
+  {
+    spotImage: {
+      type: String,
+      // required: true,
+    },
+    beachName: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      enum: [
+        "Denmark",
+        "France",
+        "Great Britain",
+        "Greece",
+        "Ireland",
+        "Italy",
+        "Netherlands",
+        "Norway",
+        "Portugal",
+        "Spain",
+      ],
+      required: true,
+    },
+    mapLink: {
+      type: String,
+      required: true,
+    },
+    skillLevel: {
+      type: String,
+      enum: ["Beginner", "Intermediate", "Advanced"],
+      required: true,
+    },
+    spotDescription: {
+      type: String,
+      required: true,
+    },
+    accessibility: {
+      type: String,
+      required: true,
+    },
+    amenities: {
+      type: String,
+      enum: ["Showers", "Toilets", "None"],
+      required: true,
+    },
+    foodSpots: {
+      type: String,
+      enum: ["Cafe", "Restaurant", "Supermarket", "Bar"],
+      required: true,
+    },
+    rating: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5],
+    },
+    typeOfSurfing: {
+      type: [String],
+      // syntax for multiple words in enum
+      enum: ["Surfing", "Body Surfing", "Body Boarding"],
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  beachName: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    enum: [
-      "Denmark",
-      "France",
-      "Great Britain",
-      "Greece",
-      "Ireland",
-      "Italy",
-      "Netherlands",
-      "Norway",
-      "Portugal",
-      "Spain",
-    ],
-    required: true,
-  },
-  mapLink: {
-    type: String,
-    required: true,
-  },
-  skillLevel: {
-    type: String,
-    enum: ["Beginner", "Intermediate", "Advanced"],
-    required: true,
-  },
-  spotDescription: {
-    type: String,
-    required: true,
-  },
-  accessibility: {
-    type: String,
-    required: true,
-  },
-  amenities: {
-    type: String,
-    enum: ["Showers", "Toilets", "None"],
-    required: true,
-  },
-  foodSpots: {
-    type: String,
-    enum: ["Cafe", "Restaurant", "Supermarket", "Bar"],
-    required: true,
-  },
-  rating: {
-    type: Number,
-    enum: [1, 2, 3, 4, 5],
-  },
-  typeOfSurfing: {
-    type: [String],
-    // syntax for multiple words in enum
-    enum: ["Surfing", "Body Surfing", "Body Boarding"],
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 const SurfSpot = model("SurfSpot", surfSpotSchema);
 module.exports = SurfSpot;
